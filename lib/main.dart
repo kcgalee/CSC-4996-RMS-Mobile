7ed6ec4d81a9310ccd31714aa4efcc0624df2f75
Anthony Jones <70544335+ajones00@users.noreply.github.com>
2022-09-20 17:59:51 -0400

    Firebase added
    Added Firebase connectivity for Android & iOS platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'; //save for later use
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   // This widget is the root of your application.
   @override
