8dc7d33314bd1df3d5a6b436098280437068b5fa
AnthonyPerfetto1 <79533313+AnthonyPerfetto1@users.noreply.github.com>
2022-09-23 17:01:41 -0400

    Completed Registration page

--- lib/login/registration.dart
+++ lib/login/registration.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant_management_system/login/registrationProcedure.dart';
 import '../patron/patronHome.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'; //save for later use
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import "package:email_validator/email_validator.dart";
+import 'registration.dart';
+
+final formKey = GlobalKey<FormState>();
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
@@ -33,6 +39,8 @@ void dispose() {
           title: const Text("Registration"),
         ),
         body: Center(
+          child: Form(
+          key: formKey,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -45,21 +53,29 @@ void dispose() {
                     prefixIcon: Icon(Icons.person, color: Colors.black),
                   ),
                 ),
-                 TextField(
+                 TextFormField(
                    controller: emailController,
                   keyboardType:TextInputType.name,
+                   autovalidateMode: AutovalidateMode.onUserInteraction,
+                   validator: (email) =>
+                   email != null && !EmailValidator.validate(email)
+                       ? 'Enter valid email' : null,
                   decoration: const InputDecoration(
                     hintText: "Email",
                     prefixIcon: Icon(Icons.mail, color: Colors.black),
-                  ),
+                    ),
+
                 ),
-                 TextField(
+                 TextFormField(
                    controller: pwController,
                   keyboardType:TextInputType.name,
                   decoration: const InputDecoration(
                     hintText: "Password",
                     prefixIcon: Icon(Icons.lock, color: Colors.black),
                   ),
+                   autovalidateMode: AutovalidateMode.onUserInteraction,
+                   validator: (value) => value != null && value.length < 6
+                   ? 'Password must be at least 6 characters' : null,
                 ),
                 TextField(
                   controller: confirmPwController,
@@ -71,27 +87,75 @@ void dispose() {
                 ),
                 SizedBox(
                     width: double.infinity,
-                    child: ElevatedButton(child: Text("Register"),
+                    child: ElevatedButton(child: const Text("Register"),
                       onPressed: () async {
                         if (pwController.text.trim() == confirmPwController.text.trim()){
-                           if (await registrationChecker(emailController.text.trim(),
-                                pwController.text.trim(), nameController.text.trim())) {
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=> PatronHome()),
-                            );
-
-                            }//end of embedded if statement
-
-                              }//end of if statement
+                           await registrationChecker(emailController.text.trim(),
+                                pwController.text.trim(), nameController.text.trim());
+                        }
                         else{
                           print("That's incorrect");
                         }
-
-                      },
+                        },
                     )
                 )
               ],
             )
         )
+        )
     );
   }
+
+
+//=====================================
+//Adding email and password to database
+//=====================================
+
+
+    registrationChecker(String email, String password, String name) async {
+      //run dart pub add email_validator in terminal to add dependencies
+      //validate e-mail
+
+      try {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+            email: email, password: password);
+
+        String userID = FirebaseAuth.instance.currentUser?.uid as String;
+        newUserData(email, userID, name);
+        return true;
+      } //end of try block
+      on FirebaseAuthException catch (e) {
+        if (e.code == 'email-already-exists') {
+          return ('Email is being used on preexisting account');
+        }
+      } //end of catch block
+
+    } // end of registrationChecker
+
+//========================
+//Creates new user document
+//======================
+    void newUserData (String email, String UID, String name) {
+      CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+      users
+          .doc(UID)
+          .set(
+          {'email' : email,
+            'name' : name,
+            'type' : 'customer'}
+
+      );
+
+      Navigator.push(context, MaterialPageRoute(builder: (context)=> PatronHome()));
+
+    } //end of newUserData
+
+
 }
+
+
+
+
+
+
