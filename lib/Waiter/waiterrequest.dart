10d6971fe5b3da486b96fb3dba5f83829a370a3c
Anthony Jones <70544335+ajones00@users.noreply.github.com>
2022-09-24 19:25:01 -0400

    in-progress data retrieval function
    testing data retrieval using the 'users' collection

--- lib/Waiter/waiterrequest.dart
+++ lib/Waiter/waiterrequest.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:restaurant_management_system/Waiter/Utility/dialog_box.dart';
+import 'package:restaurant_management_system/Waiter/models/tables.dart';
 
 import 'Utility/request_tile.dart';
 
@@ -11,6 +13,10 @@ class WaiterRequest extends StatefulWidget {
 }
 
 class _WaiterRequestState extends State<WaiterRequest> {
+
+  List<String> tableDocList = [];
+  List<Map> set2 = [];
+
   //List of task
   List toDoList = [
     ["make tutotial", false],
@@ -29,6 +35,29 @@ class _WaiterRequestState extends State<WaiterRequest> {
     },);
   }
 
+  Widget _buildListItem(BuildContext context, document){
+    return ListTile(
+      title: Row(
+        children: [
+          Expanded(
+              child: Text(
+                document['type'],
+              ),
+          ),
+          Container(
+            /*decoration: const BoxDecoration(
+              color: Color(0xffddddff),
+            ),*/
+              //padding: const EdgeInsets.all(10.0),
+              child: Text(
+                document['description'],
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +70,7 @@ class _WaiterRequestState extends State<WaiterRequest> {
         onPressed: createNewTask,
         child: Icon(Icons.add),
       ),
-      body: ListView.builder(
+      /*body: ListView.builder(
         itemCount: toDoList.length,
         itemBuilder: (context,index){
           return RequestTile(
@@ -50,7 +79,48 @@ class _WaiterRequestState extends State<WaiterRequest> {
               onChanged: (value) => checkBoxChanged(value, index),
           );
         },
+      ),*/
+      body: FutureBuilder(
+        future: getTables(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          return ListView.builder(
+              itemCount: tableDocList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(tableDocList[index]),
+                );
+              },
+          );
+        },
       ),
     );
+  }
+
+  /* //tester function
+  Future getTables() async {
+    await FirebaseFirestore.instance.collection('users').get().then(
+          (snapshot) =>
+          snapshot.docs.forEach(
+                  (element) {
+                print(element.reference);
+                //set2.add(element);
+                set2.add(element as Map<String, dynamic>);
+                print(set2);
+                print('hello');
+              }),
+    );
+  }*/
+
+  //original
+  Future getTables() async {
+    await FirebaseFirestore.instance.collection('users').get().then(
+          (snapshot) => snapshot.docs.forEach(
+              (element) {
+            print(element.reference);
+            //set2.add(element);
+            tableDocList.add(element['name']);
+          }),
+    );
+    //tableDocList.add(element.reference.id); to get doc id
   }
 }
