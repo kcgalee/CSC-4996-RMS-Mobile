526843172699a5a3c04d7a43f03b12cc57265952
Anthony Jones <70544335+ajones00@users.noreply.github.com>
2022-09-25 17:22:17 -0400

    waiter able to retrieve all tables at restaurant

--- lib/Waiter/waiterrequest.dart
+++ lib/Waiter/waiterrequest.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:restaurant_management_system/Waiter/Utility/dialog_box.dart';
 import 'package:restaurant_management_system/Waiter/models/tables.dart';
@@ -13,9 +14,10 @@ class WaiterRequest extends StatefulWidget {
 }
 
 class _WaiterRequestState extends State<WaiterRequest> {
+  var waiterRID;
+  //final Stream queryTables = FirebaseFirestore.instance.collection('tables').where('restaurantID', isEqualTo: waiterRID).snapshots();
 
   List<String> tableDocList = [];
-  List<Map> set2 = [];
 
   //List of task
   List toDoList = [
@@ -33,29 +35,6 @@ class _WaiterRequestState extends State<WaiterRequest> {
     showDialog(context: context, builder: (context){
       return DialogBox();
     },);
-  }
-
-  Widget _buildListItem(BuildContext context, document){
-    return ListTile(
-      title: Row(
-        children: [
-          Expanded(
-              child: Text(
-                document['type'],
-              ),
-          ),
-          Container(
-            /*decoration: const BoxDecoration(
-              color: Color(0xffddddff),
-            ),*/
-              //padding: const EdgeInsets.all(10.0),
-              child: Text(
-                document['description'],
-              ),
-            ),
-        ],
-      ),
-    );
   }
 
   @override
@@ -79,46 +58,70 @@ class _WaiterRequestState extends State<WaiterRequest> {
               onChanged: (value) => checkBoxChanged(value, index),
           );
         },
-      ),*/
+      ),*//*
       body: FutureBuilder(
         future: getTables(),
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        builder: (context, snapshot) {
           return ListView.builder(
               itemCount: tableDocList.length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  title: Text(tableDocList[index]),
+                  title: Text(tableDocList[index].toString()),
                 );
               },
           );
         },
-      ),
+      ),*/
+        body: FutureBuilder(
+            future: getRID(),
+            builder: (context, snapshot) {
+                return StreamBuilder(
+                    stream: FirebaseFirestore.instance.collection('tables').where('restaurantID', isEqualTo: waiterRID).snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.data == null) {
+                        return Center(
+                            child: CircularProgressIndicator()
+                        );
+                      } else {
+                        return ListView.builder(
+                            itemCount: snapshot.data?.docs.length,
+                            itemBuilder: (context, index) {
+                              var text = 'Max Capacity: ' + (snapshot.data?.docs[index]['maxCapacity'].toString() ?? '') +
+                                  '\nCurrent Capacity: ' + (snapshot.data?.docs[index]['currentCapacity'].toString() ?? '');
+                              return ListTile(
+                                title: Text(
+                                    snapshot.data?.docs[index]['description']),
+                                subtitle: Text(text),
+                              );
+                            }
+                        );
+                      }
+                    });
+              })
+        );
+  }
+
+  Future getRID() async {
+    await FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser?.uid).get().then(
+              (element) {
+            print(element.reference);
+            //item.fromFirestore();
+            //set2.add(element);
+            waiterRID = element['restaurantID'].toString();
+            print(waiterRID);
+          }
     );
   }
 
-  /* //tester function
-  Future getTables() async {
-    await FirebaseFirestore.instance.collection('users').get().then(
-          (snapshot) =>
-          snapshot.docs.forEach(
-                  (element) {
-                print(element.reference);
-                //set2.add(element);
-                set2.add(element as Map<String, dynamic>);
-                print(set2);
-                print('hello');
-              }),
-    );
-  }*/
-
   //original
   Future getTables() async {
-    await FirebaseFirestore.instance.collection('users').get().then(
+    await FirebaseFirestore.instance.collection('tables').get().then(
           (snapshot) => snapshot.docs.forEach(
               (element) {
             print(element.reference);
+            //item.fromFirestore();
             //set2.add(element);
-            tableDocList.add(element['name']);
+            tableDocList.add(element.reference.id.toString());
           }),
     );
     //tableDocList.add(element.reference.id); to get doc id
