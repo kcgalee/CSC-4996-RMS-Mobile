ec01bbb71b767c3d6a365d07e049b159012118a3
Anthony Jones <70544335+ajones00@users.noreply.github.com>
2022-09-27 18:55:41 -0400

    Removing unneeded code

--- lib/login/mainscreen.dart
+++ lib/login/mainscreen.dart
@@ -1,8 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import '../Waiter/waiterHome.dart';
-import '../patron/patronHome.dart';
 import 'registration.dart';
 import 'login.dart';
 
@@ -20,12 +16,10 @@ class MainScreen extends StatefulWidget {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(icon: const Icon(Icons.menu), onPressed: () {
-
         }),
         title: const Text("MainScreen"),
         actions: <Widget>[
           IconButton(icon: const Icon(Icons.menu), onPressed: () {
-
           }),
         ],
       ),
@@ -36,7 +30,7 @@ class MainScreen extends StatefulWidget {
           children: [
             ElevatedButton(
                 onPressed: () =>
-                    redirect(),
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>const Login())),
                 child: const Text('Login',
                   style: TextStyle(
                     color: Colors.white,
@@ -56,44 +50,7 @@ class MainScreen extends StatefulWidget {
             ),
           ],
         )
-
-
       ),
     );
-  }
-
-  redirect() async {
-    final userID = FirebaseAuth.instance.currentUser?.uid;
-    print(userID);
-
-    if (userID == null){
-      print('not logged in');
-      Navigator.push(context, MaterialPageRoute(builder: (context)=> Login()));
-    }
-
-    String acctType = "";
-
-    final docRef = FirebaseFirestore.instance.collection('users').doc(userID);
-    await docRef.get().then(
-            (DocumentSnapshot doc){
-          final data = doc.data() as Map<String, dynamic>;
-          setState(() => acctType = data['type']);
-          //print(data['type']);
-        }
-    );
-
-    if (!mounted) return;
-    if (acctType == 'customer'){
-      Navigator.push(context, MaterialPageRoute(builder: (context)=> PatronHome()));
-    } else if (acctType == 'waiter') {
-      Navigator.push(context, MaterialPageRoute(builder: (context)=> WaiterHome()));
-    } else if (acctType == 'manager'){
-      print('manager');
-    } else if (acctType == 'admin'){
-      print('admin');
-    } else {
-      Navigator.push(context, MaterialPageRoute(builder: (context)=> Login()));
-    }
-
   }
 }
