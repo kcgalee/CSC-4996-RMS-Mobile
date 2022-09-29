a46e8aa1873cb9623227f091771f3d20f272339e
Humayun  Rahman <33917230+Humayunrahman@users.noreply.github.com>
2022-09-29 16:32:27 -0400

    added Clock in/out and QR scanner button added styling for button to look like figma design

--- lib/Waiter/waiterhome.dart
+++ lib/Waiter/waiterhome.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:restaurant_management_system/Waiter/qrScannerWaiter.dart';
 import 'package:restaurant_management_system/Waiter/waiterTables.dart';
-import 'package:restaurant_management_system/Waiter/waiterrequest.dart';
+import 'package:restaurant_management_system/Waiter/waiterRequest.dart';
 
 import '../login/mainscreen.dart';
 
@@ -16,53 +17,158 @@ class _WaiterHomeState extends State<WaiterHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Waiter Home"),
-      ),
-      body: Center(
-      child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
+        appBar: AppBar(
+          title: const Text("Waiter Home"),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 1,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 26),
+                child: Text("Waiter Name",
+                style: TextStyle(fontSize: 30,),),
+              ),
 
-      ElevatedButton(
-          child: Text("Waiter Request Page"),
-      style: ElevatedButton.styleFrom(
+              Padding(
+                padding: const EdgeInsets.only(bottom: 26),
+                child: ElevatedButton(
+                  child: const Text('CLOCK OUT',),
+                  style: ElevatedButton.styleFrom(
 
-      ),
-          onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(
-                  builder: (context) => WaiterRequest()));
-      },
-      ),
+                    fixedSize: Size(330, 56),
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black54,
+                    side: BorderSide(
+                      color: Colors.black38,
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  onPressed: () {},
 
+                ),
+              ),
 
-      ElevatedButton(
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>const WaiterTables()),
-              );
-              },
-          child: const Text('Assign Tables',
-            style: TextStyle(
-              color: Colors.white,),
-            )
-      ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 26),
+                child: ElevatedButton(
+                  child: const Text('CLOCK IN',),
+                  style: ElevatedButton.styleFrom(
 
-      ElevatedButton(child: Text("sign out"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut();
-            Navigator.push(context,
-                MaterialPageRoute(
-                    builder: (context) => MainScreen()));
-          }),
+                    fixedSize: Size(330, 56),
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black54,
+                    side: BorderSide(
+                      color: Colors.black38,
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  onPressed: () {},
 
+                ),
+              ),
 
+              Padding(
+                padding: const EdgeInsets.only(bottom: 26),
+                child: ElevatedButton(
+                    child: const Text('ASSIGNED TABLES',),
+                    style: ElevatedButton.styleFrom(
 
-    ], //Children
-      ),
-      )
-    );
+                      fixedSize: Size(330, 56),
+                      textStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black54,
+                      side: BorderSide(
+                        color: Colors.black38,
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const WaiterTables()),
+                      );
+                    },
+
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(bottom: 26),
+                child: ElevatedButton(
+                  child: Text("REQUESTS"),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(20),
+                    fixedSize: Size(330, 56),
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black54,
+                    side: BorderSide(
+                      color: Colors.black38,
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => WaiterRequest()));
+                  },
+                ),
+              ),
+
+              ElevatedButton(
+                child: Text("SCAN QR CODE"),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(20),
+                  fixedSize: Size(330, 56),
+                  textStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  ),
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black54,
+                  side: BorderSide(
+                    color: Colors.black38,
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => QRScannerWaiter()));
+                },
+              ),
+              ElevatedButton(
+                  child: Text("sign out"),
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => MainScreen()));
+                  }),
+            ], //Children
+          ),
+        ));
   }
 }
-
