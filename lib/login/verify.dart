87fea983b235694394762fc3ab1d78a5ae880c2a
AnthonyPerfetto1 <79533313+AnthonyPerfetto1@users.noreply.github.com>
2022-10-01 14:20:14 -0400

    Added Verify.dart and email verification
    Also changed ItemID to itemName

---
+++ lib/login/verify.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:restaurant_management_system/patron/patronHome.dart';
+
+class VerifyScreen extends StatefulWidget {
+  @override
+  _VerifyScreenState createState() => _VerifyScreenState();
+} //class Verify Screen
+
+class _VerifyScreenState extends State<VerifyScreen> {
+  final auth = FirebaseAuth.instance;
+  late User user;
+  late Timer timer;
+
+  @override
+  void initState(){
+    user = auth.currentUser!;
+    user.sendEmailVerification();
+    
+    timer = Timer.periodic(Duration(seconds: 3), (timer) {
+      checkEmailVerified();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose(){
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text
+          ("Verification email sent to ${user.email} please verify."),),
+    );
+  }
+
+
+  Future<void> checkEmailVerified() async {
+    user = auth.currentUser!;
+    await user.reload();
+    if (user.emailVerified){
+      timer.cancel();
+      Navigator.of(context).
+      pushReplacement(MaterialPageRoute(builder: (context) =>PatronHome()));
+
+    }
+  }
+
+}
