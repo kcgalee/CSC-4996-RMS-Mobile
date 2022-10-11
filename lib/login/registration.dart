0559d85864c1fcec8f7252b8e88aea99ab68a027
Kylie Lee <kcgalee@gmail.com>
2022-10-11 15:12:22 -0400

    Style Login + Registration Page

--- lib/login/registration.dart
+++ lib/login/registration.dart
@@ -328,7 +328,22 @@ class RegistrationBodyState extends State<RegistrationBody> {
               ),
               SizedBox(
                   width: double.infinity,
-                  child: ElevatedButton(child: const Text("Register"),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(330, 56),
+                      textStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                      backgroundColor: Colors.black87,
+                      foregroundColor: Colors.white,
+                      side: BorderSide(
+                        color: Colors.black38,
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    child: const Text("Register"),
                     onPressed: () async {
                       if (pwController.text.trim() ==
                           confirmPwController.text.trim()) {
