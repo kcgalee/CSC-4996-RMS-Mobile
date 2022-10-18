be2c15953e586a7f448db422d8e4e2fc75bbd05c
AnthonyPerfetto1 <79533313+AnthonyPerfetto1@users.noreply.github.com>
2022-10-18 15:14:34 -0400

    Add employee is now fully functional

--- lib/manager/addEmployee.dart
+++ lib/manager/addEmployee.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:restaurant_management_system/manager/manageEmployee.dart';
 import 'Utility/MangerNavigationDrawer.dart';
@@ -181,23 +182,25 @@ class _AddEmployee extends State<AddEmployee> {
       String lastName, String preferredName, String managerID) async {
     //run dart pub add email_validator in terminal to add dependencies
     //validate e-mail
-
+      FirebaseApp app = await Firebase.initializeApp(
+        name: "Secondary", options: Firebase.app().options);
     try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: email, password: password);
+      UserCredential userCredential = await FirebaseAuth.instanceFor(app: app)
+          .createUserWithEmailAndPassword(email: email, password: password);
 
-      String userID = FirebaseAuth.instance.currentUser?.uid as String;
+      String? userID = userCredential.user?.uid.toString();
+      newUserData(email, userID.toString().trim(), firstName, lastName, preferredName, managerID);
 
-
-      newUserData(email, userID, firstName, lastName, preferredName, managerID);
-      return true;
     } //end of try block
     on FirebaseAuthException catch (e) {
       if (e.code == 'email-already-exists') {
+        await app.delete();
         return ('Email is being used on preexisting account');
       }
     } //end of catch block
-
+    await app.delete();
+      return Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const ManageEmployee()));
   }
 
   void newUserData(String email, String UID, String firstName,
@@ -222,8 +225,7 @@ class _AddEmployee extends State<AddEmployee> {
 
     );
 
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => const ManageEmployee()));
+
   }
 
 }
