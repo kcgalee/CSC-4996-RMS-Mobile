1fd9328af7f42089311112ef45f2cf311358adc6
Kylie Lee <kcgalee@gmail.com>
2022-10-18 22:42:26 -0400

    Styled Menu Items

--- lib/customer/showMenuItems.dart
+++ lib/customer/showMenuItems.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:restaurant_management_system/manager/editRestaurant.dart';
 import 'package:counter/counter.dart';
 import 'Models/createOrderInfo.dart';
-
+import 'package:restaurant_management_system/widgets/customSubButton.dart';
 
 
 class ShowMenuItems extends StatefulWidget {
@@ -31,9 +31,10 @@ class _ShowMenuItems extends State<ShowMenuItems> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.lightBlue[100],
         appBar: AppBar(
           title: Text('Menu Items'),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
           elevation: 0,
         ),
         body: StreamBuilder(
@@ -50,48 +51,70 @@ class _ShowMenuItems extends State<ShowMenuItems> {
                 return ListView.builder(
                     itemCount: snapshot.data?.docs.length,
                     itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(snapshot.data?.docs[index]['name'] ?? ''),
-                        subtitle: Text(snapshot.data?.docs[index]['price'] ?? ''),
+                      return  Padding(
+                          padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+                          child: Container(
+                              height: 70.0,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  border: Border.all(
+                                    color: Colors.black,
+                                    width: 1.0,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.grey ,
+                                        blurRadius: 2.0,
+                                        offset: Offset(2.0,2.0)
+                                    )
+                                  ]
+                              ),
+                              child: ListTile(
+                                title: Text(snapshot.data?.docs[index]['name'] ?? ''),
+                                subtitle: Text(snapshot.data?.docs[index]['price'] ?? ''),
 
-                        onTap: () {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: Text(snapshot.data?.docs[index]['name']),
-                                content: Text(snapshot.data?.docs[index]['description'] +
-                                "\n" + snapshot.data?.docs[index]['price']),
-                                actions: <Widget>[
-                                  TextButton(
-                                    child:  const Text("Cancel"),
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
+                                onTap: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: Text(snapshot.data?.docs[index]['name']),
+                                        content: Text(snapshot.data?.docs[index]['description'] +
+                                            "\n" + snapshot.data?.docs[index]['price']),
+                                        actions: <Widget>[
+                                          TextButton(
+                                            child:  const Text("Cancel"),
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                          Container(
+                                            child: Counter(
+                                              min: 0,
+                                              max: 10,
+                                              bound: 0,
+                                              step: 1,
+                                              onValueChanged: print,
+                                            ),
+                                          ),
+                                          TextButton(
+                                            child:  const Text("Add to Order"),
+                                            onPressed: () {
+                                              int count = 1;
+                                              createOrderInfo.setter(snapshot.data?.docs[index].id as String, count);
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                        ],
+                                      );
                                     },
-                                  ),
-                                  Container(
-                                    child: Counter(
-                                      min: 0,
-                                      max: 10,
-                                      bound: 0,
-                                      step: 1,
-                                      onValueChanged: print,
-                                    ),
-                                  ),
-                                  TextButton(
-                                    child:  const Text("Add to Order"),
-                                    onPressed: () {
-                                      int count = 1;
-                                      createOrderInfo.setter(snapshot.data?.docs[index].id as String, count);
-                                      Navigator.of(context).pop();
-                                    },
-                                  ),
-                                ],
-                              );
-                            },
-                          );
-                        },
+                                  );
+                                },
+                              )
+                          )
                       );
+
                     }
                 );
               }
