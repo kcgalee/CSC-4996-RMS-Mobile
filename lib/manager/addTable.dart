44fbcb94c1a7bd7594fef631156d4ea220e5af30
Humayun  Rahman <33917230+Humayunrahman@users.noreply.github.com>
2022-10-20 15:22:20 -0400

    did styling for add table and added Table Location on from

--- lib/manager/addTable.dart
+++ lib/manager/addTable.dart
@@ -5,6 +5,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:restaurant_management_system/widgets/customMainButton.dart';
+import 'package:restaurant_management_system/widgets/customSubButton.dart';
 
 import 'GenerateQRCode.dart';
 import 'Utility/MangerNavigationDrawer.dart';
@@ -23,6 +25,7 @@ class _AddTable extends State<AddTable> {
   final tableNumberController = TextEditingController();
   final tableCapacityController = TextEditingController();
   final tableTypeController = TextEditingController();
+  final tableLocationController = TextEditingController();
   int numb = 0;
   late Map documents;
 
@@ -31,52 +34,90 @@ class _AddTable extends State<AddTable> {
       drawer: const ManagerNavigationDrawer(),
       appBar: AppBar(
         title: Text("Add Table"),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 1,
       ),
       body: Center(
-  child: Column(
-  children: [
+  child: SingleChildScrollView(
+    padding: const EdgeInsets.all(24),
+    child: Column(
+    children: [
 
-    TextFormField(
-      controller: tableNumberController,
-      keyboardType: TextInputType.number,
-      decoration: const InputDecoration(
-        hintText: "Table number",
-        prefixIcon: Icon(Icons.numbers, color: Colors.black),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 15),
+        child: TextFormField(
+          controller: tableNumberController,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            hintText: "Table number",
+            prefixIcon: Icon(Icons.numbers, color: Colors.black),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 2),
+              ),
+              border: OutlineInputBorder()
+          ),
+        ),
       ),
-    ),
-    TextFormField(
 
-      controller: tableCapacityController,
-      keyboardType: TextInputType.number,
-      decoration: const InputDecoration(
-        hintText: "Table Capacity",
-        prefixIcon: Icon(Icons.people, color: Colors.black),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 15),
+        child: TextFormField(
+          controller:tableLocationController,
+          keyboardType: TextInputType.text,
+          decoration: const InputDecoration(
+            hintText: "Table Location",
+            prefixIcon: Icon(Icons.location_on, color: Colors.black),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 2),
+              ),
+              border: OutlineInputBorder()
+          ),
+        ),
       ),
-    ),
 
-
-    TextFormField(
-      controller: tableTypeController,
-      keyboardType: TextInputType.name,
-      decoration: const InputDecoration(
-        hintText: "Table Type",
-        prefixIcon: Icon(Icons.type_specimen, color: Colors.black),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 15),
+        child: TextFormField(
+          controller: tableCapacityController,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            hintText: "Table Capacity",
+            prefixIcon: Icon(Icons.people, color: Colors.black),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 2),
+              ),
+              border: OutlineInputBorder()
+          ),
+        ),
       ),
-    ),
-    SizedBox(
-      width: double.infinity,
-      child: ElevatedButton(child: const Text("Add Table"),
+
+
+      Padding(
+        padding: const EdgeInsets.only(bottom: 15),
+        child: TextFormField(
+          controller: tableTypeController,
+          keyboardType: TextInputType.name,
+          decoration: const InputDecoration(
+            hintText: "Table Type",
+            prefixIcon: Icon(Icons.table_bar, color: Colors.black),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 2),
+              ),
+              border: OutlineInputBorder()
+          ),
+        ),
+      ),
+
+      CustomMainButton(
+        text: "Add Table",
         onPressed: () =>
-            newTableData(int.parse(tableNumberController.text.trim()),
-                int.parse(tableCapacityController.text.trim()),
-                tableTypeController.text.trim()),
-      ),
-    )
+        newTableData(int.parse(tableNumberController.text.trim()),
+            int.parse(tableCapacityController.text.trim()),
+            tableTypeController.text.trim()),)
 
-
-
-
-  ]),
+    ]),
+  ),
       )
 
 
