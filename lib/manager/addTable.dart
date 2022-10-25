77fb3d9138904b8bdd7af9181d96ea17096dde45
AnthonyPerfetto1 <79533313+AnthonyPerfetto1@users.noreply.github.com>
2022-10-25 11:29:18 -0400

    added category header to showMenuItems

--- lib/manager/addTable.dart
+++ lib/manager/addTable.dart
@@ -138,7 +138,7 @@ class _AddTable extends State<AddTable> {
           'restID': text,
           'type': tableType,
           'waiterID': '',
-          'available': true,
+          'waiterName' : '',
           'currentCapacity': 0,
           'location' : location
         }
