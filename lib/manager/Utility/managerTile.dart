672948c44134aa302bccbcba183055296b2c66f9
Humayun  Rahman <33917230+Humayunrahman@users.noreply.github.com>
2022-11-04 18:40:04 -0400

    bugfix + recommit Slidable widget

--- lib/manager/Utility/managerTile.dart
+++ lib/manager/Utility/managerTile.dart
@@ -1,12 +1,13 @@
 
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 
 
 class ManagerTile extends StatelessWidget {
-  VoidCallback onPressedEdit;
-  VoidCallback onPressedDelete;
+  Function(BuildContext) onPressedEdit;
+  Function(BuildContext) onPressedDelete;
   final String taskName;
    String subTitle;
    final itemIMG;
@@ -27,66 +28,52 @@ class ManagerTile extends StatelessWidget {
         builder: (context, snapshot) {
           return Padding(
             padding: const EdgeInsets.only(left: 15, right: 15,bottom: 25),
-            child: Container(
-              padding: const EdgeInsets.only(right: 5,left: 15,bottom: 10,top: 10),
-              decoration: BoxDecoration(color: Colors.grey[100],
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all(color: Colors.black54,width: 2)
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            child: Slidable(
+              endActionPane: ActionPane(
+                motion:const StretchMotion(),
                 children: [
-                  Text(taskName,
-                      style: const TextStyle(color: Colors.black54,fontSize: 15, fontWeight: FontWeight.bold)),
-
-                  Text(subTitle,
-                      style: const TextStyle(color: Colors.black54,fontSize: 15)),
-
-                  Image.network(itemIMG),
-                  
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.only(right: 40,left: 40),
-                            textStyle: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                            backgroundColor: Colors.grey[600],
-                            foregroundColor: Colors.white,
-                            side: const BorderSide(
-                              color: Colors.black38,
-                            ),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)),
-                          ),
-
-                          onPressed: onPressedEdit,
-                          child: const Text('EDIT')),
-
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.only(right: 30,left: 30),
-                            textStyle: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                            backgroundColor: Colors.red[900],
-                            foregroundColor: Colors.white,
-                            side: const BorderSide(
-                              color: Colors.black38,
-                            ),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)),
-                          ),
-
-                          onPressed: onPressedDelete,
-                          child: const Text('DELETE')),
-                    ],
+                  SlidableAction(
+                    onPressed:  onPressedEdit,
+                    icon: Icons.edit_note,
+                    label: 'EDIT',
+                    backgroundColor: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(10),
                   ),
+                  SlidableAction(
+                    onPressed: onPressedDelete,
+                    icon: Icons.delete,
+                    label: 'DELETE',
+                    backgroundColor: Colors.redAccent,
+                    borderRadius: BorderRadius.circular(10),
+                  )
+
                 ],
+              ),
+              child: Container(
+                padding: const EdgeInsets.only(right: 5,left: 15,bottom: 10,top: 10),
+                decoration: BoxDecoration(color: Colors.grey[100],
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Colors.black54,width: 2)
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(taskName,
+                              style: const TextStyle(color: Colors.black54,fontSize: 15, fontWeight: FontWeight.bold)),
+
+                          Text(subTitle,
+                              style: const TextStyle(color: Colors.black54,fontSize: 15)),
+
+                          Image.network(itemIMG),
+
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
@@ -97,64 +84,47 @@ class ManagerTile extends StatelessWidget {
         builder: (context, snapshot) {
           return Padding(
             padding: const EdgeInsets.only(left: 15, right: 15,bottom: 25),
-            child: Container(
-              padding: const EdgeInsets.only(right: 5,left: 15,bottom: 10,top: 10),
-              decoration: BoxDecoration(color: Colors.grey[100],
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all(color: Colors.black54,width: 2)
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            child: Slidable(
+              endActionPane: ActionPane(
+                motion:const StretchMotion(),
                 children: [
-                  Text(taskName,
-                      style: const TextStyle(color: Colors.black54,fontSize: 15, fontWeight: FontWeight.bold)),
-
-                  Text(subTitle,
-                      style: const TextStyle(color: Colors.black54,fontSize: 15)),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.only(right: 40,left: 40),
-                            textStyle: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                            backgroundColor: Colors.grey[600],
-                            foregroundColor: Colors.white,
-                            side: const BorderSide(
-                              color: Colors.black38,
-                            ),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)),
-                          ),
-
-                          onPressed: onPressedEdit,
-                          child: const Text('EDIT')),
-
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.only(right: 30,left: 30),
-                            textStyle: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                            backgroundColor: Colors.red[900],
-                            foregroundColor: Colors.white,
-                            side: const BorderSide(
-                              color: Colors.black38,
-                            ),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)),
-                          ),
-
-                          onPressed: onPressedDelete,
-                          child: const Text('DELETE')),
-                    ],
+                  SlidableAction(
+                    onPressed: onPressedEdit,
+                    icon: Icons.edit_note,
+                    label: 'EDIT',
+                    backgroundColor: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(10),
                   ),
+                  SlidableAction(
+                    onPressed: onPressedDelete,
+                    icon: Icons.delete,
+                    label: 'DELETE',
+                    backgroundColor: Colors.redAccent,
+                    borderRadius: BorderRadius.circular(10),
+                  )
+
                 ],
+              ),
+              child: Container(
+                padding: const EdgeInsets.only(right: 5,left: 15,bottom: 10,top: 10),
+                decoration: BoxDecoration(color: Colors.grey[100],
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Colors.black54,width: 2)
+                ),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(taskName,
+                            style: const TextStyle(color: Colors.black54,fontSize: 15, fontWeight: FontWeight.bold)),
+
+                        Text(subTitle,
+                            style: const TextStyle(color: Colors.black54,fontSize: 15)),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           );
