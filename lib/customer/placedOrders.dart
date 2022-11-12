372a343a5bc03f670e93d221d74187bac16ce16d
Kylie Lee <kcgalee@gmail.com>
2022-11-12 15:46:01 -0500

    Add Item Info Pop Up on Placed Orders

--- lib/customer/placedOrders.dart
+++ lib/customer/placedOrders.dart
@@ -364,6 +364,33 @@ class _PlacedOrders extends State<PlacedOrders> {
                                             }
 
                                       },
+                                        onTap: () {
+                                        showDialog<void>(
+                                          context: context,
+                                          barrierDismissible: false, // user must tap button!
+                                          builder: (BuildContext context) {
+                                            return AlertDialog(
+                                              title: const Text('AlertDialog Title'),
+                                              content: SingleChildScrollView(
+                                                child: ListBody(
+                                                  children: const <Widget>[
+                                                    Text('This is a demo alert dialog.'),
+                                                    Text('Would you like to approve of this message?'),
+                                                  ],
+                                                ),
+                                              ),
+                                              actions: <Widget>[
+                                                TextButton(
+                                                  child: const Text('Approve'),
+                                                  onPressed: () {
+                                                    Navigator.of(context).pop();
+                                                  },
+                                                ),
+                                              ],
+                                            );
+                                          },
+                                        );
+                                      },
                                       );
 
 
