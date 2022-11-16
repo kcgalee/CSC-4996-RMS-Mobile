fbd156426dd9793ab6c657089becbbd6954359a4
Humayun  Rahman <33917230+Humayunrahman@users.noreply.github.com>
2022-11-16 15:58:35 -0500

    fix

--- lib/manager/editItem.dart
+++ lib/manager/editItem.dart
@@ -6,6 +6,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:restaurant_management_system/manager/manageMenuItem.dart';
 import 'package:restaurant_management_system/widgets/customMainButton.dart';
 import 'package:restaurant_management_system/widgets/customTextForm.dart';
 import '../widgets/customBackButton.dart';
@@ -264,7 +265,7 @@ class _EditItem extends State<EditItem> {
                             Divider(color: Colors.white,thickness: 1,indent: 10,endIndent: 10,),
                             ListTile(
                               leading: const Icon(Icons.camera_alt,color: Colors.white,),
-                              title: const Text('Camera',style: TextStyle(color: Colors.white)),
+                              title: const Text('Camara',style: TextStyle(color: Colors.white)),
                               onTap: () => pickImage(ImageSource.camera),
                             ),
                           ],
