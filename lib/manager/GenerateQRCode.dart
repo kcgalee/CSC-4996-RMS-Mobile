661a32ee49ad507d80cc77a2a7a59c63094edf20
Humayun  Rahman <33917230+Humayunrahman@users.noreply.github.com>
2022-12-03 04:24:07 -0500

    added back button

--- lib/manager/GenerateQRCode.dart
+++ lib/manager/GenerateQRCode.dart
@@ -35,7 +35,10 @@ class _GenerateQRCode extends State<GenerateQRCode> {
       child: Column(
         children: [
           //back button
- 
+          CustomBackButton(
+              onPressed: () {
+                Navigator.pop(context);
+              }),
           Padding(
             padding: const EdgeInsets.only(top: 60),
             child: QrImage(
